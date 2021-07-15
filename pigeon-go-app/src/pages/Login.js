6e8af04af74e007f1895b7ef72ea87847fe38ce7
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-15 21:52:52 +0530

    style: login page

--- pigeon-go-app/src/pages/Login.js
+++ pigeon-go-app/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import { Button } from '@material-ui/core';
 import logo from "../images/logo.jpg"
@@ -7,23 +7,42 @@ import { useStateValue } from "../StateProvider"
 
 function Login() {
 
-        const [ ,dispatch] = useStateValue()
+    const [, dispatch] = useStateValue()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-        const signInHandler = () => {
-            auth.signInWithPopup(provider).then(result => {dispatch({
+    const emailSignInHandler = () => {
+        auth.signInWithEmailAndPassword(email, password).then(result => {
+
+        })
+    }
+
+    const googleSignInHandler = () => {
+        auth.signInWithPopup(provider).then(result => {
+            dispatch({
                 type: "SET_USER",
                 user: result.user
-            })})
-        }
-
-        return (
-            <div className="login">
-                <div className="login__container -flex">
-                    <img className="login__image" src={logo} alt="logo" />
-                    <Button onClick={signInHandler} className="login__button --cursor">Sign In with Google</Button>
-                </div>
-            </div>
-        )
+            })
+        })
     }
+
+    return (
+        <div className="login -flex">
+            <img className="login__image" src={logo} alt="logo" />
+            <div className="login__container -flex">
+                <h1 className="f-b">Sign In</h1>
+                <form className="-flex login__from">
+                    <p className="mb-05 f-b">Email</p>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} className="mb-3 p-03" type="email" placeholder="Enter email" />
+                    <p className="mb-05 f-b">Password</p>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} className="mb-3 p-03" type="password" placeholder="Enter password" />
+                    <Button onClick={emailSignInHandler} className="mb-3 login__button login__emailSignIn" type="submit">Sign In</Button>
+                    <small className="t-j mb-3">By signing-in you agree to refrain from defamatory, indecent, offensive, profane, discriminatory, misleading, unlawful or threatening comments.</small>
+                    <Button onClick={googleSignInHandler} className="login__button login__googleSignIn --cursor">Sign In with Google</Button>
+                </form>
+            </div>
+        </div>
+    )
+}
 
 export default Login
